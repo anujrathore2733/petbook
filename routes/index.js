87f28103abe6223c8b30982d2689496cc8c9c07a
Siddharth <siddharthjazz123@gmail.com>
2020-01-24 02:55:58 +0530

    done2

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ router.get('/getin',function(req,res,next){
   res.render('getin.hbs',{signup_success:req.query.signup_success,user_not_found:req.query.user_not_found,invalid_password:req.query.invalid_password,dup_email:req.query.dup_email})
 })
 
+router.get('/Aboutus', function(req,res){
+  res.render('Aboutus.hbs')
+})
+
 router.post('/signup',controller.signup)
 
 router.post('/login',controller.login)
